e31cec2a906b36ef973c8a9bcc16f3b86db8c572
Stuart Halloway <stu@thinkrelevance.com>
2012-10-23 07:51:55 -0400

    query against a group of attributes

---
+++ tutorial/attribute_groups.clj
@@ -0,0 +1,42 @@
+;; You can use rules to develop queries that match the same criterion
+;; against a group of attributes
+(use :reload 'datomic.samples.repl)
+(easy!)
+(def conn (scratch-conn))
+
+(transact-all conn (io/resource "day-of-datomic/social-news.dtm"))
+
+;; find all attributes in the story namespace
+(q '[:find ?e
+     :in $
+     :where
+     [?e :db/valueType]
+     [?e :db/ident ?a]
+     [(namespace ?a) ?ns]
+     [(= ?ns "story")]]
+   (db conn))
+
+;; create a reusable rule
+(def rules
+  '[[[attr-in-namespace ?e ?ns2]
+     [?e :db/ident ?a]
+     [?e :db/valueType]
+     [(namespace ?a) ?ns1]
+     [(= ?ns1 ?ns2)]]])
+
+;; find all attributes in story namespace, using the rule
+(q '[:find ?e
+     :in $ %
+     :where
+     (attr-in-namespace ?e "story")]
+   (db conn) rules)
+
+;; find all entities possessing *any* story attribute
+(q '[:find ?e
+     :in $ %
+     :where
+     [?e ?a]
+     (attr-in-namespace ?a "story")]
+   (db conn) rules)
+
+
