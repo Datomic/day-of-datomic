9cc74355872d032723a04efd010e17f4c0ef46a6
Bobby Calderwood <bobby_calderwood@mac.com>
2013-01-31 15:38:14 -0500

    Update to datomic-free 0.8.3767 and clojure 1.5.0-RC4

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject day-of-datomic "1.0.0-SNAPSHOT"
   :description "Sample Code for Day of Datomic Presentation"
   :plugins [[lein-tg "0.0.1"]]
-  :dependencies [[org.clojure/clojure "1.5.0-RC1"]
+  :dependencies [[org.clojure/clojure "1.5.0-RC4"]
                  [org.clojure/test.generative "0.3.0"]
-                 [com.datomic/datomic-free "0.8.3731"]
+                 [com.datomic/datomic-free "0.8.3767"]
                  [incanter/incanter-charts "1.3.0"]
                  [incanter/incanter-pdf "1.3.0"]])
