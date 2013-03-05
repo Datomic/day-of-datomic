9beadde6bf07ef3616da8a428140730a64544514
Bobby Calderwood <bobby_calderwood@mac.com>
2013-03-05 17:01:39 -0500

    Update to Clojure 1.5.0

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject day-of-datomic "1.0.0-SNAPSHOT"
   :description "Sample Code for Day of Datomic Presentation"
   :plugins [[lein-tg "0.0.1"]]
-  :dependencies [[org.clojure/clojure "1.5.0-RC17"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.clojure/test.generative "0.3.0"]
                  [com.datomic/datomic-free "0.8.3826"]
                  [incanter/incanter-charts "1.3.0"]
