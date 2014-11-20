b6689e705c8b49f106d1b66325298209ec3fcecd
Stuart Halloway <stu@cognitect.com>
2014-11-20 13:37:21 -0500

    explicit VM settings

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject day-of-datomic "1.0.0-SNAPSHOT"
   :description "Sample Code for Day of Datomic Presentation"
   :plugins [[lein-tg "0.0.1"]]
+  :jvm-opts ["-Xmx1g" "-server"]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/test.generative "0.3.0"]
                  [com.datomic/datomic-free "0.9.5067"]
