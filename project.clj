42667a80fedc44601c2030eb7fa7523ac12af9c7
Chris Redinger <redinger@gmail.com>
2015-03-10 18:48:22 -0400

    Update Datomic version

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :jvm-opts ["-Xmx1g" "-server"]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/test.generative "0.3.0"]
-                 [com.datomic/datomic-free "0.9.5151"]
+                 [com.datomic/datomic-free "0.9.5152"]
                  [incanter/incanter-charts "1.3.0"]
                  [incanter/incanter-pdf "1.3.0"]])
