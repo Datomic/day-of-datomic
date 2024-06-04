daa457f766e16f55243a95513e759573b8827329
Keith Harper <keithharper1155@gmail.com>
2024-06-04 14:19:14 -0400

    Clojure 1.11.3

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Sample Code for Day of Datomic Presentation"
   :plugins [[lein-tg "0.0.1"]]
   :jvm-opts ["-Xmx1g" "-server"]
-  :dependencies [[org.clojure/clojure "1.11.2"]
+  :dependencies [[org.clojure/clojure "1.11.3"]
                  [org.clojure/test.generative "0.5.2"]
                  [com.datomic/peer "1.0.7075"]
                  [incanter/incanter-charts "1.9.0"]
